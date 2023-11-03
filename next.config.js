f0cfeb83cff678a7f2b695c24c987ba1e674c505
EXO <rob.farag@outlook.com>
2023-11-03 16:04:37 +1100

    Enable topLevelAwait in Next.js config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+/**
+ * @type {import('next').NextConfig}
+ */
+module.exports = {
   reactStrictMode: true,
   experimental: {
     topLevelAwait: true,
   },
 };
-
-module.exports = nextConfig;
