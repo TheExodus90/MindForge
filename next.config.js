20582bc3281f1c755808b1926ed3afd24eafc065
EXO <rob.farag@outlook.com>
2023-11-03 22:11:47 +1100

    Enable topLevelAwait in Next.js config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-/** @type {import("next").NextConfig} */
 module.exports = {
-  experimental: { appDir: true },
-  webpack(config) {
-    config.experiments = { ...config.experiments, topLevelAwait: true }
-    return config
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.experiments = { topLevelAwait: true };
+    }
+    return config;
   },
-}
+};
