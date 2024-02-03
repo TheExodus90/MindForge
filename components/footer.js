b47b4ec15a82c967fef7ded1075925eff9615526
EXO <rob.farag@outlook.com>
2024-02-04 10:27:14 +1100

    Added contact, X profiles links on Footer

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,9 @@
 import Link from 'next/link';
-import styles from '../pages/index.module.css';// Assuming you have a CSS module for styling
+import styles from '../pages/index.module.css';
 
 function Footer() {
+    const supportEmail = 'support@exofi.io';
+
     return (
         <footer className={styles.footer}>
             <div className={styles.footerContent}>
@@ -12,6 +14,12 @@ function Footer() {
                     <Link href="/terms">
                         Terms of Service
                     </Link>
+                    <a href={`mailto:${supportEmail}`}>
+                        Contact
+                    </a>
+                    <a href="https://twitter.com/ExofiLabs" target="_blank" rel="noopener noreferrer">
+                        X
+                    </a>
                 </div>
                 <div className={styles.copyRight}>
                     <p>© {new Date().getFullYear()} ExoFi Labs. Made by ExoFi. All rights reserved.</p>
