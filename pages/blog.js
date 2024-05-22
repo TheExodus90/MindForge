9bfac90895881c56e2270a876d8d1fc6afdff90b
TheExodus90 <rob.farag@outlook.com>
2024-05-22 22:24:01 +1000

    updated blog entry 1

--- pages/blog.js
+++ pages/blog.js
@@ -17,10 +17,11 @@ const Blog = () => {
         <p>Mind Forge is a cutting-edge digital platform powered by ChatGPT, offering seamless integration of advanced AI communication with rapid voice synthesis capabilities from 11Labs and Google TTS. It's a dynamic and responsive experience that brings together the latest advancements in artificial intelligence to create an unparalleled user experience.</p>
         <h3>Key Features</h3>
         <ol>
-          <li>Personalized Conversations: Mind Forge offers a range of AI personalities to chat with, each tailored to suit your preferences and needs.</li>
-          <li>Rapid Voice Synthesis: With Mind Forge, you can turn text into speech in an instant.</li>
-          <li>Advanced AI Capabilities: Powered by ChatGPT, Mind Forge is equipped with advanced AI capabilities.</li>
+          <li>Personalized Conversations: Mind Forge offers a range of AI personalities to chat with, each tailored to suit your preferences and needs along with their own custom avatars with beautifully made artwork.</li>
+          <li>Rapid Voice Synthesis: With Mind Forge, you can turn text into speech in an instant with rapid voice generation by google TTS (Multilingual voice support coming in an update soon).</li>
+          <li>Advanced AI Capabilities: Powered by ChatGPT, Mind Forge is equipped with advanced AI capabilities and access to powerful LLM Models.</li>
           <li>Image Generation with DALL-E-3: Experience the future of image generation with DALL-E-3 integration.</li>
+          <li>Translation: Easily translate text to and from any language, with multilingual voice support for 100+ languages coming soon.</li>
         </ol>
         <h3>How Does It Work?</h3>
         <p>Using Mind Forge is simple. Just select an AI personality to chat with, type your message, and watch as Mind Forge responds with insightful, engaging, and sometimes even humorous responses.</p>
